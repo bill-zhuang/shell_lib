53cfb072e764f9d0b2806841cd8e67e1063b4688
bill-zhuang <zstu_bill@yahoo.com>
2015-01-15 11:48:15 +0800

    + update check database table php file(init adapter & parameter in construct)

--- DatabaseTableCheck.php
+++ DatabaseTableCheck.php
@@ -2,18 +2,29 @@
 
 class Database_Table_Check
 {
+    private $_show_tables_sql;
+    private $_describe_sql;
+
+    private $_db_option;
+    private $_target_adapter;
+    private $_source_adapter;
+
     public function __construct()
     {
+        $this->_show_tables_sql = 'SHOW TABLES';
+        $this->_desribe_sql = 'DESC %s';
 
+        $this->_db_option = [
+
+        ];
+        $this->_target_adapter = new PDO("mysql:host=localhost;dbname=bill", 'root', '123456', $this->_db_option);
+        $this->_source_adapter = new PDO("mysql:host=localhost;dbname=bill", 'root', '123456', $this->_db_option);
     }
 
     public function run()
     {
-        $target_adapter = new PDO("mysql:host=localhost;dbname=bill", 'root', '123456', []);
-        $source_adapter = new PDO("mysql:host=localhost;dbname=bill", 'root', '123456', []);
-        
-        $target_tables_data = $target_adapter->query('show tables')->fetchAll();
-        $source_tables_data = $source_adapter->query('show tables')->fetchAll();
+        $target_tables_data = $this->_target_adapter->query($this->_show_tables_sql)->fetchAll();
+        $source_tables_data = $this->_source_adapter->query($this->_show_tables_sql)->fetchAll();
         $target_tables = [];
         $source_tables = [];
         foreach ($target_tables_data as $target_tables_value)
@@ -26,16 +37,15 @@ class Database_Table_Check
         }
 
         echo 'Table syn check start:' . "\n\n";
-        $sql_table_definition_statement = 'desc %s';
         foreach ($target_tables as $target_table)
         {
             $table_name = $target_table;
             //echo "\t" . 'Check table ' . $table_name . ':' . "\n\n";
-            $sql_get_table_definition = sprintf($sql_table_definition_statement, $table_name);
+            $sql_get_table_definition = sprintf($this->_desribe_sql, $table_name);
             if (in_array($table_name, $source_tables))
             {
-                $target_table_definition = $target_adapter->query($sql_get_table_definition)->fetchAll();
-                $source_table_definition = $source_adapter->query($sql_get_table_definition)->fetchAll();
+                $target_table_definition = $this->_target_adapter->query($sql_get_table_definition)->fetchAll();
+                $source_table_definition = $this->_source_adapter->query($sql_get_table_definition)->fetchAll();
 
                 if (count($target_table_definition) != count($source_table_definition))
                 {
